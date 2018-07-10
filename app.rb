87316f7020c8a58c7b02ff85b1cec2eb5c8411be
Nick Brown <nickrossbrown@gmail.com>
2018-07-09 21:03:12 -0700

    use a not uet functional definition method with additional page routing

--- app.rb
+++ app.rb
@@ -12,11 +12,23 @@ end
 post('/') do
   word = params["word"]
   words = Word.new(word)
+  defin = params["definition"]
+  @definition = defin
   words.save
   @word_list = Word.all()
+  # binding.pry
   erb(:input)
 end
+
 get ('/words/:id') do
   @word = Word.find(params[:id])
+  @definition = Word.definition(params[definition])
+  erb(:output)
+end
+
+post('/word/:id') do
+  binding.pry
+  defin = params["definition"]
+  @definition = Word.definition(defin)
   erb(:output)
 end
--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,23 +1,29 @@
 class Word
   @@word_list=[]
   attr_reader :id
-  attr_accessor :word
+  attr_accessor :word, :definition
    def initialize (word)
      @word = word
      @id = @@word_list.length + 1
+     @definition = []
    end
+
    def self.all
      @@word_list
    end
+
    def add_to_list
      @@word_list.push(@word)
    end
+
    def save
       @@word_list.push(self)
   end
+
   def self.clear
     @@word_list = []
   end
+
   def self.find(id)
     word_id = id.to_i()
     @@word_list.each do |word|
@@ -25,6 +31,10 @@ class Word
         return word
       end
     end
+  end
+
+  def definition (define)
+    @definition.push(define)
   end
 
 end
